3ca1f500cb409515e1381c090279c55f4318ebe0
Art4 <art4@wlabs.de>
2016-11-26 16:44:33 +0100

    Smiles als Module exportieren

--- build_files.php
+++ build_files.php
@@ -6,5 +6,6 @@ $converter = new Youthweb\SmileyEmojiMigration\Converter();
 
 $converter->toJsonFile('dist/smilies.json');
 $converter->toJavaScriptFile('dist/smilies.js');
+$converter->toNodeModule('lib/Smileystore.js');
 
 echo "All files created. 🚀\n";
