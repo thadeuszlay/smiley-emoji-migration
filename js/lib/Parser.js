26eaed20b4e29e495632041fa20a3b3c6ebd8e1d
Art4 <art4@wlabs.de>
2017-04-25 16:56:01 +0200

    JSON_PRETTY_PRINT für PHP build verwenden

--- js/lib/Parser.js
+++ js/lib/Parser.js
@@ -1,47 +1,48 @@
-function Parser() {
-}
-Parser.prototype.match_regex = /\B([:;]{1}[+\-\(\)\[\]\w_äöü]*)$/;
+var Parser = function()
+{
+	this.match_regex = /\B([:;]{1}[+\-\(\)\[\]\w_äöü]*)$/;
 
-Parser.prototype.emoji_template = '<img alt="{alt}" class="emojione" src="{img}"/>';
+	this.emoji_template = '<img alt="{alt}" class="emojione" src="{img}"/>';
 
-Parser.prototype.parse_template = function(img, alt) {
-	return this.emoji_template
+	this.parse_template = function(img, alt) {
+		return this.emoji_template
 		.replace('{img}', img)
 		.replace('{alt}', alt);
-};
+	};
 
-Parser.prototype.shortnameToImage = function(str, template) {
-	return str.replace(/:?;?\+?\-?[\w\)\(_]+:?/g, function(shortname) {
-		var orig_shortname = shortname;
-		var unicode_obj = emojione.emojioneList[shortname];
-		if (unicode_obj) {
-			if (unicode_obj.isSmiley)
-			{
-				return template
+	this.shortnameToImage = function(str, template) {
+		return str.replace(/:?;?\+?\-?[\w\)\(_]+:?/g, function(shortname) {
+			var orig_shortname = shortname;
+			var unicode_obj = emojione.emojioneList[shortname];
+			if (unicode_obj) {
+				if (unicode_obj.isSmiley)
+				{
+					return template
 					.replace('{img}', '/vendor/smilies/'+unicode_obj.filename)
 					.replace('{alt}', orig_shortname);
-			} else {
-				unicode = unicode_obj.unicode;
-				return getTemplate(template, unicode[unicode.length-1], shortname);
+				} else {
+					unicode = unicode_obj.unicode;
+					return getTemplate(template, unicode[unicode.length-1], shortname);
+				}
 			}
-		}
-		return unicode_obj;
-	});
-};
+			return unicode_obj;
+		});
+	};
 
-Parser.prototype.shortnameToUnicode = function(emojione, str) {
-	return str.replace(/:?\+?[\w_\-]+:?/g, function(shortname) {
-		var unicode_obj = emojione.emojioneList[shortname];
-		if (unicode_obj) {
-			unicode = unicode_obj.unicode;
-			if (unicode_obj.isSmiley === true) {
-				return unicode[unicode.length-1];
+	this.shortnameToUnicode = function(emojione, str) {
+		return str.replace(/:?\+?[\w_\-]+:?/g, function(shortname) {
+			var unicode_obj = emojione.emojioneList[shortname];
+			if (unicode_obj) {
+				unicode = unicode_obj.unicode;
+				if (unicode_obj.isSmiley === true) {
+					return unicode[unicode.length-1];
+				}
+				return emojione.convert(unicode[unicode.length-1]);
 			}
-			return emojione.convert(unicode[unicode.length-1]);
-		}
-		return unicode_obj;
-	});
-};
+			return unicode_obj;
+		});
+	};
+}
 
 module.exports = Parser;
 var Parser = new Parser();
